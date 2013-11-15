14a8fd20ff268bf72af243d4e4ded860b035f5c8
thanatos6666 <jonathan.c.li@gmail.com>
2013-11-15 12:17:17 +1100

    Part 6 done

--- js/main.js
+++ js/main.js
@@ -1,10 +1,6 @@
 var app = {
 
-	renderHomeView: function() {
-		$('body').html(this.homeTpl());
-		$('.search-key').on('keyup', $.proxy(this.findByName, this));
-	},
-
+	
     showAlert: function (message, title) {
 		if (navigator.notification) {
 			navigator.notification.alert(message, null, title, 'OK');
@@ -13,24 +9,13 @@ var app = {
 		}
 	},
 	
-	findByName: function() {
-        var self = this;
-		this.store.findByName($('.search-key').val(), function(employees) {
-			$('.employee-list').html(self.employeeLiTpl(employees));
-		});
-    },
+	
 
     initialize: function() {
 		var self = this;
-
-		this.homeTpl = Handlebars.compile($("#home-tpl").html());
-		this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
-
-        this.store = new MemoryStore(function() {
-
-			self.renderHomeView();
+		this.store = new MemoryStore(function() {
+			$('body').html(new HomeView(self.store).render().el);
 		});
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
 };
