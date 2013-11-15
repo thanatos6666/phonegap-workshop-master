f1db2c2f7d51fb3c1bbf1871b931d622a4d4e859
thanatos6666 <jonathan.c.li@gmail.com>
2013-11-15 12:39:35 +1100

    Revert "Trying routing"
    This reverts commit 766bdd7a27d0b040485de92546b891285bb42399.

--- js/main.js
+++ js/main.js
@@ -9,23 +9,8 @@ var app = {
 		}
 	},
 	
-	route: function() {
-		var hash = window.location.hash;
-		if (!hash) {
-			$('body').html(new HomeView(this.store).render().el);
-			return;
-		}
-		var match = hash.match(app.detailsURL);
-		if (match) {
-			this.store.findById(Number(match[1]), function(employee) {
-				$('body').html(new EmployeeView(employee).render().el);
-			});
-		}
-	}
-
 	registerEvents: function() {
 		var self = this;
-		
 		// Check of browser supports touch events...
 		if (document.documentElement.hasOwnProperty('ontouchstart')) {
 			// ... if yes: register touch event listener to change the "selected" state of the item
@@ -44,16 +29,13 @@ var app = {
 				$(event.target).removeClass('tappable-active');
 			});
 		}
-
-		$(window).on('hashchange', $.proxy(this.route, this));
 	},
 
     initialize: function() {
 		var self = this;
 		this.registerEvents();
-		this.detailsURL = /^#employees\/(\d{1,})/;
 		this.store = new MemoryStore(function() {
-			self.route();
+			$('body').html(new HomeView(self.store).render().el);
 		});
 		
     }
